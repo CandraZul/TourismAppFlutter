67346c120a938cb870ab97798fd3454b58e423d9
Candra Zulkarnain <candrazulkarnain8@gmail.com>
2025-01-17 21:51:19 +0700

    add bookmark with setState method

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tourism_app/model/tourism.dart';
 import 'package:tourism_app/screen/detail/detail_screen.dart';
 import 'package:tourism_app/screen/home/home_screen.dart';
+import 'package:tourism_app/screen/main/main_screen.dart';
 import 'package:tourism_app/static/navigation_route.dart';
 import 'package:tourism_app/style/theme/tourism_theme.dart';
 
@@ -16,16 +17,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      initialRoute: NavigationRoute.homeRoute.name,
+      initialRoute: NavigationRoute.mainRoute.name,
       theme: TourismTheme.lightTheme,
       darkTheme: TourismTheme.darkTheme,
       themeMode: ThemeMode.system,
-      routes: {
-        NavigationRoute.homeRoute.name: (context) => const HomeScreen(),
-        NavigationRoute.detailRoute.name: (context) => DetailScreen(
-              tourism: ModalRoute.of(context)?.settings.arguments as Tourism,
-            ),
-      },
+           routes: {
+       NavigationRoute.mainRoute.name: (context) => const MainScreen(),
+       NavigationRoute.detailRoute.name: (context) => DetailScreen(
+             tourism: ModalRoute.of(context)?.settings.arguments as Tourism,
+           ),
+     },
     );
   }
 }
